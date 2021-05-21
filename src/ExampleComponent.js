0aa8adb6691ec8bcba4535f075b2c4633280c7a9
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-05-21 08:00:15 -0400

    done

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -8,8 +8,11 @@ class ExampleComponent extends Component {
         <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
         <p>Whoa!</p>
       </div>
+      
+      
     );
   }
 }
 
 export default ExampleComponent;
+
